0ecead3c33cec614fe5684bffbc52cbf6cccec78
matdomichard <mathieu.domichard@gmail.com>
2023-01-20 00:10:56 +0100

    Ajout du UserService

--- src/main/java/CompteAr/backend/controller/UserController.java
+++ src/main/java/CompteAr/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package CompteAr.backend.controller;
 
 import CompteAr.backend.model.User;
 import CompteAr.backend.repository.UserRepository;
+import CompteAr.backend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,37 +18,37 @@ import java.util.Optional;
 public class UserController {
 
         @Autowired
-        private  UserRepository userRepository;
+        private  UserService userService;
 
         @GetMapping
         public List<User> getAllUsers() {
-                return userRepository.findAll();
+                return userService.getAllUsers();
         }
 
         @PostMapping
         public ResponseEntity<User> createUser(@RequestBody User user) {
-                Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+                Optional<User> existingUser = userService.findByEmail(user.getEmail());
                 if (existingUser.isPresent()) {
                         return new ResponseEntity<>(HttpStatus.CONFLICT);
                 }
-                User savedUser = userRepository.save(user);
+                User savedUser = userService.save(user);
                 return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
         }
 
         @GetMapping("/{id}")
         public ResponseEntity<User> getUserById(@PathVariable Integer id) {
-                return userRepository.findById(id)
+                return userService.findById(id)
                         .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
                         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
         }
 
         @PutMapping("/{id}")
         public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User userDetails) {
-                return userRepository.findById(id)
+                return userService.findById(id)
                         .map(user -> {
                                 user.setPassword(userDetails.getPassword());
                                 user.setEmail(userDetails.getEmail());
-                                final User updatedUser = userRepository.save(user);
+                                final User updatedUser = userService.save(user);
                                 return new ResponseEntity<>(updatedUser, HttpStatus.OK);
                         })
                         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -55,8 +56,8 @@ public class UserController {
 
         @DeleteMapping("/{id}")
         public ResponseEntity deleteUser(@PathVariable Integer id) {
-                if (userRepository.existsById(id)) {
-                        userRepository.deleteById(id);
+                if (userService.existsById(id)) {
+                        userService.deleteById(id);
                         return new ResponseEntity<>(HttpStatus.OK);
                 } else {
                         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
