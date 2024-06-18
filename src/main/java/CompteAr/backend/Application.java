586fd78224b9f936774bc787bb6cd65b5ecd491a
Mathieu Domichard <mathieu.domichard@gmail.com>
2024-06-18 17:31:23 +0200

    Ajout de port pour heroku

--- src/main/java/CompteAr/backend/Application.java
+++ src/main/java/CompteAr/backend/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
+import java.util.Collections;
+
 import CompteAr.backend.config.OpenAPIConfig;
 
 @SpringBootApplication
@@ -19,7 +21,13 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+		app.run(args);
 	}
 
+	private static String getPort() {
+		String port = System.getenv("PORT");
+		return port != null ? port : "8080";
+	}
 }
