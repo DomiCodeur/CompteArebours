05c9a6931b215dc5f0fa5f596d9b730c0c0a5ad7
mdomichard <mathieu.domichard@gmail.com>
2025-02-11 15:01:49 +0100

    ajout du autowired instanciation des services

--- src/main/java/CompteAr/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/CompteAr/backend/config/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,9 +26,11 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-  private final JwtService jwtService;
-  private final UserDetailsService userDetailsService;
+	
+	@Autowired
+  private  JwtService jwtService;
+	@Autowired
+  private  UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(
